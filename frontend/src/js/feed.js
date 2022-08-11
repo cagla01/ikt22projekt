e835a1cda0acc56889f75a45c4676e905e004734
cagla01 <caglak770@gmail.com>
2022-08-11 13:50:18 +0200

    caching

--- frontend/src/js/feed.js
+++ frontend/src/js/feed.js
@@ -1,6 +1,7 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+let shareImageButton = document.querySelector('#share-image-button');
+let createPostArea = document.querySelector('#create-post');
+let closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+let sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
     createPostArea.style.display = 'block';
@@ -13,3 +14,33 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard() {
+    let cardWrapper = document.createElement('div');
+    cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+    cardWrapper.style.width = '250px';
+    let cardTitle = document.createElement('div');
+    cardTitle.className = 'mdl-card__title';
+    cardTitle.style.backgroundImage = 'url("/src/images/food1.jpeg")';
+    cardTitle.style.backgroundSize = 'cover';
+    cardTitle.style.height = '325px';
+    cardWrapper.appendChild(cardTitle);
+    let cardTitleTextElement = document.createElement('h2');
+    cardTitleTextElement.className = 'mdl-card__title-text';
+    cardTitle.appendChild(cardTitleTextElement);
+    let cardSupportingText = document.createElement('div');
+    cardSupportingText.className = 'mdl-card__supporting-text';
+    cardSupportingText.textContent = 'Food Test 1';
+    cardSupportingText.style.textAlign = 'center';
+    cardWrapper.appendChild(cardSupportingText);
+    componentHandler.upgradeElement(cardWrapper);
+    sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(data) {
+        createCard();
+    });
