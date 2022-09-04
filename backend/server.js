9148dc4d7e7e39cc7cd08cb7053c089791fe4b2e
cagla01 <caglak770@gmail.com>
2022-09-04 16:36:09 +0200

    funktionalitäten in posts

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const postsRoutes = require('./routes/posts.routes');
-const uploadRoutes = require('./routes/upload.routes');
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-// enable cors for all requests
 app.use(cors());
-app.use('/posts', postsRoutes);
-app.use('/img', uploadRoutes);
+app.use('/posts', postsRoutes)
 
 app.listen(PORT, (error) => {
-    if (error) {
-        console.log(error);
+    if(error) {
+        console.log(error)
     } else {
         console.log(`server running on http://localhost:${PORT}`);
     }
-});
+})
 
-// connect to mongoDB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+/* die folgende Verbindung brauchen wir gar nicht, wird jeweils bei Bedarf erzeugt (mongoose) */
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(
+        () => console.log('connected to BD')
+    ).catch(
+    err => console.error(err, 'conncetion error')
+)
+
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log('connected to DB');
-});
