1321e057e525e4558aef26e861a5a326c3dee84a
cagla01 <caglak770@gmail.com>
2022-09-08 19:32:51 +0200

    indexeddb erstellt

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,25 +1,44 @@
+importScripts('/src/js/idb.js');
+
 const CACHE_VERSION = 3;
 const CURRENT_STATIC_CACHE = 'static-v'+CACHE_VERSION;
 const CURRENT_DYNAMIC_CACHE = 'dynamic-v'+CACHE_VERSION;
+
+const STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/js/idb.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
+];
+
+const db = idb.openDB('posts-store', 1, {
+    upgrade(db) {
+        // Create a store of objects
+        const store = db.createObjectStore('posts', {
+            // The '_id' property of the object will be the key.
+            keyPath: '_id',
+            // If it isn't explicitly set, create a value by auto incrementing.
+            autoIncrement: true,
+        });
+        // Create an index on the '_id' property of the objects.
+        store.createIndex('_id', '_id');
+    },
+});
 
 self.addEventListener('install', event => {
     console.log('service worker --> installing ...', event);
     event.waitUntil(
         caches.open(CURRENT_STATIC_CACHE)
-            .then( cache => {
+            .then(cache => {
                 console.log('Service-Worker-Cache erzeugt und offen');
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
-                ]);
+                cache.addAll(STATIC_FILES);
             })
     );
 })
@@ -45,21 +64,44 @@ self.addEventListener('fetch', event => {
     // if request is made for web page url must contains http.
     if (!(event.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
 
-    event.respondWith(
-        caches.match(event.request)
-            .then( response => {
-                if(response) {
-                    return response;
-                } else {
-                    return fetch(event.request)
-                        .then( res => {     // nicht erneut response nehmen, haben wir schon
-                            return caches.open(CURRENT_DYNAMIC_CACHE)      // neuer, weiterer Cache namens dynamic
-                                .then( cache => {
-                                    cache.put(event.request.url, res.clone());
-                                    return res;
-                                })
-                        });
-                }
-            })
-    );
+    const url = 'http://localhost:3000/posts';
+    if(event.request.url.indexOf(url) >= 0) {
+        event.respondWith(
+            fetch(event.request)
+                .then ( res => {
+                    const clonedResponse = res.clone();
+                    clonedResponse.json()
+                        .then( data => {
+                            for(let key in data)
+                            {
+                                db
+                                    .then( dbPosts => {
+                                        let tx = dbPosts.transaction('posts', 'readwrite');
+                                        let store = tx.objectStore('posts');
+                                        store.put(data[key]);
+                                        return tx.done;
+                                    })
+                            }
+                        })
+                    return res;
+                })
+        )
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then( response => {
+                    if(response) {
+                        return response;
+                    } else {
+                        return fetch(event.request)
+                            .then( res => {     // nicht erneut response nehmen, haben wir schon
+                                return caches.open(CURRENT_DYNAMIC_CACHE)      // neuer, weiterer Cache namens dynamic
+                                    .then( cache => {
+                                        cache.put(event.request.url, res.clone());
+                                        return res;
+                                    })
+                            });
+                    }
+                })
+        )}
 })
